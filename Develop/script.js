1bca101d347e6d0feca0ba16a10a0dd14fdac049
Camille Mei Rong <70561939+meiquery@users.noreply.github.com>
2021-03-14 00:05:51 -0500

    Add files via upload
    cleaning up unused id's and classes, refactoring styling, broke some things along the way

--- Develop/script.js
+++ Develop/script.js
@@ -1,26 +1,31 @@
 // elements from DOM
 var generateBtn = document.querySelector("#generate");
-var password = document.querySelector(".card-body");
-var result = document.getElementById("result");
-var length = document.getElementById("length");
-var lower = document.getElementById("result");
-var upper = document.getElementById("result");
-var number = document.getElementById("result");
-var special = document.getElementById("result");
+var password = document.getElementsByClassName(".card-body");
 
-const randomFunction = {
+
+var result = document.getElementById("result");
+//var length = document.getElementById("length");
+var lower = document.getElementById("setLower");
+var upper = document.getElementById("setUpper");
+var number = document.getElementById("setNumeric");
+var special = document.getElementById("setSpecial");
+
+
+ /* const randomFunction = {
   lower: getRandomLC,
   upper: getRandomUC,
   number: getRandomNum,
   special: getRandomSpecial
-};
+
+  element.addEventListener("click", function(){ alert("Hello World!"); });
+}; */
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-generateBtn.addEventListener("click", showPassword);
+generateBtn.addEventListener("click", writePassword());
+generateBtn.addEventListener("click", showPassword());
 
 
-//randomization functions
+//randomization functions 
 function getRandomLC() {
  return String.fromCharCode(Math.floor(Math.random() *26) + 97);
 }
@@ -30,29 +35,46 @@ function getRandomUC() {
 }
 
 function getRandomNum() {
- return String.fromCharCode(Math.floor(Math.random() *10 + 48);
+ return String.fromCharCode(Math.floor(Math.random() *10) + 48);
 }
+
 function getRandomSpecial() {
  var special= "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
  return symbols(Math.floor(Math.random() * s));
 }
 
+function allRandom() {
+  getRandomLC();
+  getRandomUC();
+  getRandomNum();
+  getRandomSpecial();
+};
+
 
 
 // prints slider value to screen
 function rangeSlide(value) {
-  document.getElementById("length").innerHTML = value;
+  document.getElementById("howlong").innerHTML = value;
 };
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-  passwordText.value = password;
-};
 
 //generated password appears!
 function showPassword() {
-password.style.display = "block";
+   password.style.display = "block";
 };
+
+
+// Write password to the #password input
+function writePassword() {
+  var passwordText = document.querySelector("#passcode");
+  //passwordText.value = password;
+
+  console.log(typeof length);
+};
+
+
+rangeSlide();
+
+
+
 
